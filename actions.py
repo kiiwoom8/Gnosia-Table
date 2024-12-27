e26686f225f5a63f4b16d18ea2936e3e1e882056
Zane <noerror96@gmail.com>
2024-12-27 12:51:24 +0900

    change the sequnce of votes in a tie situation

--- actions.py
+++ actions.py
@@ -32,60 +32,77 @@ def release_ties():
     if data.ties:
         for char_index in data.ties:
             char_name = data.characters[char_index]
-            if char_name in data.words_to_color:
+            if char_name in data.words_to_color and data.words_to_color[char_name] == data.YELLOW:
                 del data.words_to_color[char_name]
         data.ties = []
+        data.voting_characters = {}
 
 def handle_vote(action_choice, action_name):
     while True:
         t.check_error()
         t.t_print("1. \033[91mStart the vote!\033[0m")
         t.t_print("2. \033[31mVote\033[0m") 
-        t.t_print("3. \033[92mRelease current votes\033[0m")
+        t.t_print("3. \033[31mFreeze All\033[0m") 
+        t.t_print("4. \033[34mFreeze Nobody\033[0m") 
         t.t_print("z. Go back")
         
         vote_menu_choice = t.t_input("Select an action by number: ")
         match vote_menu_choice:
             case '1':
-                votes = {}
                 while True:
-                    for char_index, char_name in data.characters.items():
+                    data.voting_characters = data.voting_characters if data.voting_characters else list(data.characters.keys())
+                    print(data.voting_characters)
+                    voting_characters = data.voting_characters.copy()
+                    for char_index in voting_characters:
+                        char_name = data.characters[char_index]
                         if char_name != " "  and data.words_to_color.get(char_name) not in [data.BLUE, data.RED]:
                             target = get_target(char_index)
                             if target == 'z':
                                 return False
                             if target != 'p':
                                 target = int(target)
-                                votes[target] = votes.get(target, 0) + 1
+                                data.votes[target] = data.votes.get(target, 0) + 1
                                 record_action(action_choice, action_name, char_index, target)
-                    max_votes = max(votes.values(), default=0)
-                    most_voted = [char for char, votes in votes.items() if votes == max_votes]
+                        data.voting_characters.remove(char_index)
+                    max_votes = max(data.votes.values(), default=0)
+                    most_voted = sorted([char for char, votes in data.votes.items() if votes == max_votes])
+                    data.votes = {}
                     if len(most_voted) == 1:
                         release_ties()
-                        t.r_print(f"{data.characters[most_voted[0]]} is {data.BLUE}cold sleeped!{data.RESET}")
-                        data.words_to_color[data.characters[most_voted[0]]] = data.BLUE
+                        toggle_role(most_voted[0], 10)
                     else:
-                        release_ties()
-                        data.ties = most_voted
-                        t.r_print("\033[91mIt's a tie! Vote again.\033[0m")
-                        for char in most_voted:
-                            data.words_to_color[data.characters[char]] = data.YELLOW
+                        set_ties(most_voted)
                     table_rendering.print_table()
                     return
             case '2':
                 record_action(action_choice, action_name)
             case '3':
-                if not data.ties:
-                    t.error_text = "\033[31mNo votes to release.\033[0m"
-                else:
+                if data.ties:
+                    for char in data.ties:
+                        toggle_role(char, 10)
                     release_ties()
-                    t.r_print("\033[92mCharacters voted tie are sucessfully released.\033[0m")
+                else:
+                    t.error_text = "\033[31mNo votes to freeze.\033[0m"
+            case '4':
+                if data.ties:
+                    release_ties()
+                    t.r_print("\033[34mNobody is frozen.\033[0m")
+                else:
+                    t.error_text = "\033[31mNo votes to release.\033[0m"
             case 'z':
                 return
             case '':
                 pass
             case _:
                 t.error_text = "\033[31mInvalid choice. Try again.\033[0m"
+def set_ties(most_voted):
+    data.ties = most_voted
+    t.r_print("\033[91mIt's a tie! Vote again.\033[0m")
+    data.voting_characters = list(data.characters.keys())
+    for char in reversed(data.ties):
+        data.voting_characters.remove(char)
+        data.voting_characters.insert(1, char)
+        data.words_to_color[data.characters[char]] = data.YELLOW
 
 def get_target(actor):
     while True:
--- data.py
+++ data.py
@@ -3,7 +3,7 @@ import additional_functions
 
 RED, BLUSH, LBLUE, BLUE, YELLOW, RESET = "\033[31m", "\033[91m", "\033[94m", "\033[34m", "\033[33m", "\033[0m"
 
-characters, numbered_characters, removed_characters, current_roles = {}, {}, {}, {}
+characters, numbered_characters, removed_characters, votes, voting_characters, current_roles = {}, {}, {}, {}, {}, {}
 matrix, words_to_color, ties, notes, history = [], [], [], [], []
 table = ""
 
